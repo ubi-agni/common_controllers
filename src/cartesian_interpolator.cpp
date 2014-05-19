0d82e0e3e914336e0512b94e2098174a90d0dbc9
Tomasz Winiarski <tomrobotics@gmail.com>
2014-05-19 17:18:12 +0200

    properties added to interpolator

--- src/cartesian_interpolator.cpp
+++ src/cartesian_interpolator.cpp
@@ -13,11 +13,17 @@
 #include "rtt_rosclock/rtt_rosclock.h"
 #include "Eigen/Geometry"
 
-CartesianInterpolator::CartesianInterpolator(const std::string& name) :
-  RTT::TaskContext(name), trajectory_ptr_(0) {
+CartesianInterpolator::CartesianInterpolator(const std::string& name)
+    : RTT::TaskContext(name),
+      trajectory_ptr_(0),
+      activate_pose_init_property_(false) {
   this->ports()->addPort("CartesianPosition", port_cartesian_position_);
   this->ports()->addPort("CartesianPositionCommand", port_cartesian_command_);
   this->ports()->addPort("CartesianTrajectoryCommand", port_trajectory_);
+
+  this->addProperty("activate_pose_init_property",
+                    activate_pose_init_property_);
+  this->addProperty("init_setpoint_property", init_setpoint_property_);
 }
 
 CartesianInterpolator::~CartesianInterpolator() {
@@ -28,8 +34,13 @@ bool CartesianInterpolator::configureHook() {
 }
 
 bool CartesianInterpolator::startHook() {
-  if (port_cartesian_position_.read(setpoint_) == RTT::NoData) {
-    return false;
+
+  if (activate_pose_init_property_) {
+    setpoint_ = init_setpoint_property_;
+  } else {
+    if (port_cartesian_position_.read(setpoint_) == RTT::NoData) {
+      return false;
+    }
   }
 
   return true;
@@ -45,7 +56,7 @@ void CartesianInterpolator::updateHook() {
   if (trajectory_ && (trajectory_->header.stamp < now)) {
     for (; trajectory_ptr_ < trajectory_->points.size(); trajectory_ptr_++) {
       ros::Time trj_time = trajectory_->header.stamp
-                           + trajectory_->points[trajectory_ptr_].time_from_start;
+          + trajectory_->points[trajectory_ptr_].time_from_start;
       if (trj_time > now) {
         break;
       }
@@ -58,27 +69,34 @@ void CartesianInterpolator::updateHook() {
         p0.pose = old_point_;
         setpoint_ = interpolate(p0, trajectory_->points[trajectory_ptr_], now);
       } else {
-        setpoint_ = interpolate(trajectory_->points[trajectory_ptr_-1], trajectory_->points[trajectory_ptr_], now);
+        setpoint_ = interpolate(trajectory_->points[trajectory_ptr_ - 1],
+                                trajectory_->points[trajectory_ptr_], now);
       }
     }
   }
   port_cartesian_command_.write(setpoint_);
 }
 
-geometry_msgs::Pose CartesianInterpolator::interpolate(const cartesian_trajectory_msgs::CartesianTrajectoryPoint& p0,
-                                                       const cartesian_trajectory_msgs::CartesianTrajectoryPoint& p1,
-                                                       ros::Time t) {
+geometry_msgs::Pose CartesianInterpolator::interpolate(
+    const cartesian_trajectory_msgs::CartesianTrajectoryPoint& p0,
+    const cartesian_trajectory_msgs::CartesianTrajectoryPoint& p1,
+    ros::Time t) {
   geometry_msgs::Pose pose;
 
   ros::Time t0 = trajectory_->header.stamp + p0.time_from_start;
   ros::Time t1 = trajectory_->header.stamp + p1.time_from_start;
 
-  pose.position.x = interpolate(p0.pose.position.x, p1.pose.position.x, t0.toSec(), t1.toSec(), t.toSec());
-  pose.position.y = interpolate(p0.pose.position.y, p1.pose.position.y, t0.toSec(), t1.toSec(), t.toSec());
-  pose.position.z = interpolate(p0.pose.position.z, p1.pose.position.z, t0.toSec(), t1.toSec(), t.toSec());
+  pose.position.x = interpolate(p0.pose.position.x, p1.pose.position.x,
+                                t0.toSec(), t1.toSec(), t.toSec());
+  pose.position.y = interpolate(p0.pose.position.y, p1.pose.position.y,
+                                t0.toSec(), t1.toSec(), t.toSec());
+  pose.position.z = interpolate(p0.pose.position.z, p1.pose.position.z,
+                                t0.toSec(), t1.toSec(), t.toSec());
 
-  Eigen::Quaterniond q0(p0.pose.orientation.w, p0.pose.orientation.x, p0.pose.orientation.y, p0.pose.orientation.z);
-  Eigen::Quaterniond q1(p1.pose.orientation.w, p1.pose.orientation.x, p1.pose.orientation.y, p1.pose.orientation.z);
+  Eigen::Quaterniond q0(p0.pose.orientation.w, p0.pose.orientation.x,
+                        p0.pose.orientation.y, p0.pose.orientation.z);
+  Eigen::Quaterniond q1(p1.pose.orientation.w, p1.pose.orientation.x,
+                        p1.pose.orientation.y, p1.pose.orientation.z);
 
   double a = interpolate(0.0, 1.0, t0.toSec(), t1.toSec(), t.toSec());
   Eigen::Quaterniond q = q0.slerp(a, q1);
@@ -90,6 +108,7 @@ geometry_msgs::Pose CartesianInterpolator::interpolate(const cartesian_trajector
   return pose;
 }
 
-double CartesianInterpolator::interpolate(double p0, double p1, double t0, double t1, double t) {
-  return (p0 + (p1 - p0) * (t - t0)/(t1 - t0));
+double CartesianInterpolator::interpolate(double p0, double p1, double t0,
+                                          double t1, double t) {
+  return (p0 + (p1 - p0) * (t - t0) / (t1 - t0));
 }
