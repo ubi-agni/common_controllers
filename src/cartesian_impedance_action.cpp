0508013a7236f66d809c3383c0659142402edaa7
Konrad Banachowicz <konradb3@gmail.com>
2014-05-28 10:50:26 +0200

    implement proper action interface for CartesianImpedanceAction.

--- src/cartesian_impedance_action.cpp
+++ src/cartesian_impedance_action.cpp
@@ -16,12 +16,23 @@ using ::cartesian_trajectory_msgs::CartesianImpedanceTrajectoryConstPtr;
 CartesianImpedanceAction::CartesianImpedanceAction(const std::string& name) : RTT::TaskContext(name) {
   this->ports()->addPort("CartesianImpedanceTrajectoryCommand", port_cartesian_trajectory_command_);
   this->ports()->addPort("impedance", port_cartesian_trajectory_);
+  
+  as_.addPorts(this->provides());
+
+  as_.registerGoalCallback(boost::bind(&CartesianImpedanceAction::goalCB, this, _1));
+  as_.registerCancelCallback(boost::bind(&CartesianImpedanceAction::cancelCB, this, _1));
 }
 
 CartesianImpedanceAction::~CartesianImpedanceAction() {
 }
 
 bool CartesianImpedanceAction::startHook() {
+  if (as_.ready()) {
+    as_.start();
+  } else {
+    return false;
+  }
+
   return true;
 }
 
@@ -35,4 +46,112 @@ void CartesianImpedanceAction::updateHook() {
 
     port_cartesian_trajectory_command_.write(trj_cptr);
   }
+  
+  if (active_goal_.isValid() && (active_goal_.getGoalStatus().status == actionlib_msgs::GoalStatus::ACTIVE)) {
+    Goal g = active_goal_.getGoal();
+    size_t last_point = g->trajectory.points.size() - 1;
+
+    if ((g->trajectory.header.stamp + g->trajectory.points[last_point].time_from_start) < rtt_rosclock::host_now()) {
+      active_goal_.setSucceeded();
+    }
+  }
 }
+
+void CartesianImpedanceAction::goalCB(GoalHandle gh) {
+  // cancel active goal
+  if (active_goal_.isValid() && (active_goal_.getGoalStatus().status == actionlib_msgs::GoalStatus::ACTIVE)) {
+    active_goal_.setCanceled();
+  }
+
+  Goal g = gh.getGoal();
+
+  bool valid = true;
+
+  for (size_t i = 0; i < g->trajectory.points.size(); i++) {
+    valid = valid && checkImpedance(g->trajectory.points[i].impedance);
+  }
+
+  if (!valid) {
+    cartesian_trajectory_msgs::CartesianImpedanceResult res;
+    res.error_code = cartesian_trajectory_msgs::CartesianImpedanceResult::INVALID_GOAL;
+    gh.setRejected(res);
+  }
+
+  CartesianImpedanceTrajectory* trj_ptr =  new CartesianImpedanceTrajectory;
+  *trj_ptr = g->trajectory;
+  CartesianImpedanceTrajectoryConstPtr trj_cptr = CartesianImpedanceTrajectoryConstPtr(trj_ptr);
+  
+  if (g->trajectory.header.stamp < rtt_rosclock::host_now()) {
+    valid = false;
+    cartesian_trajectory_msgs::CartesianImpedanceResult res;
+    res.error_code = cartesian_trajectory_msgs::CartesianImpedanceResult::OLD_HEADER_TIMESTAMP;
+    gh.setRejected(res);
+  }
+  
+  if (valid) {
+    port_cartesian_trajectory_command_.write(trj_cptr);
+
+    gh.setAccepted();
+    active_goal_ = gh;
+  }
+}
+
+void CartesianImpedanceAction::cancelCB(GoalHandle gh) {
+  if (active_goal_ == gh) {
+    port_cartesian_trajectory_command_.write(CartesianImpedanceTrajectoryConstPtr());
+    active_goal_.setCanceled();
+  }
+}
+
+bool CartesianImpedanceAction::checkImpedance(const cartesian_trajectory_msgs::CartesianImpedance& imp) {
+  if(imp.stiffness.force.x > 4000.0 || imp.stiffness.force.x < 0.0) {
+    return false;
+  }
+
+  if(imp.stiffness.force.y > 4000.0 || imp.stiffness.force.y < 0.0) {
+    return false;
+  }
+
+  if(imp.stiffness.force.z > 4000.0 || imp.stiffness.force.z < 0.0) {
+    return false;
+  }
+
+  if(imp.stiffness.torque.x > 300.0 || imp.stiffness.torque.x < 0.0) {
+    return false;
+  }
+
+  if(imp.stiffness.torque.y > 300.0 || imp.stiffness.torque.y < 0.0) {
+    return false;
+  }
+
+  if(imp.stiffness.torque.z > 300.0 || imp.stiffness.torque.z < 0.0) {
+    return false;
+  }
+  
+  if(imp.damping.force.x > 1.0 || imp.damping.force.x < 0.0) {
+    return false;
+  }
+
+  if(imp.damping.force.y > 1.0 || imp.damping.force.y < 0.0) {
+    return false;
+  }
+
+  if(imp.damping.force.z > 1.0 || imp.damping.force.z < 0.0) {
+    return false;
+  }
+
+  if(imp.damping.torque.x > 1.0 || imp.damping.torque.x < 0.0) {
+    return false;
+  }
+
+  if(imp.damping.torque.y > 1.0 || imp.damping.torque.y < 0.0) {
+    return false;
+  }
+
+  if(imp.damping.torque.z > 1.0 || imp.damping.torque.z < 0.0) {
+    return false;
+  }
+  
+  return true;
+}
+
--- src/cartesian_impedance_action.h
+++ src/cartesian_impedance_action.h
@@ -14,8 +14,18 @@
 #include "rtt/TaskContext.hpp"
 #include "rtt/Port.hpp"
 #include "cartesian_trajectory_msgs/CartesianImpedanceTrajectory.h"
+#include "cartesian_trajectory_msgs/CartesianImpedance.h"
+#include "cartesian_trajectory_msgs/CartesianImpedanceAction.h"
+#include "cartesian_trajectory_msgs/CartesianImpedanceGoal.h"
+
+#include "rtt_actionlib/rtt_actionlib.h"
+#include "rtt_actionlib/rtt_action_server.h"
 
 class CartesianImpedanceAction: public RTT::TaskContext {
+ private:
+  typedef actionlib::ServerGoalHandle<cartesian_trajectory_msgs::CartesianImpedanceAction> GoalHandle;
+  typedef boost::shared_ptr<const cartesian_trajectory_msgs::CartesianImpedanceGoal> Goal;
+
  public:
   explicit CartesianImpedanceAction(const std::string& name);
   virtual ~CartesianImpedanceAction();
@@ -24,8 +34,15 @@ class CartesianImpedanceAction: public RTT::TaskContext {
   void updateHook();
 
  private:
+  void goalCB(GoalHandle gh);
+  void cancelCB(GoalHandle gh);
+ 
+  bool checkImpedance(const cartesian_trajectory_msgs::CartesianImpedance& imp);
+ 
   RTT::OutputPort<cartesian_trajectory_msgs::CartesianImpedanceTrajectoryConstPtr> port_cartesian_trajectory_command_;
   RTT::InputPort<cartesian_trajectory_msgs::CartesianImpedanceTrajectory> port_cartesian_trajectory_;
+  rtt_actionlib::RTTActionServer<cartesian_trajectory_msgs::CartesianImpedanceAction> as_;
+  GoalHandle active_goal_;
 };
 
 #endif  // CARTESIAN_IMPEDANCE_ACTION_H_
